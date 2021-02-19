93e3ff5d6308b74e2b431d50afde2c5b46e7154b
MakeShiftArtist <lrmcbride4@gmail.com>
2021-02-19 00:54:22 -0600

    added 8ball command

--- src/handler/commands/fun.ts
+++ src/handler/commands/fun.ts
@@ -7,7 +7,7 @@ import {
     Infos
 } from '@typeit/discord'
 
-import { User } from 'discord.js'
+import { User, MessageEmbed } from 'discord.js'
 
 /**
  * Default imports
@@ -23,7 +23,7 @@ export default abstract class fun extends baseclass {
 
     /**
      * Iterates over the member list and tells you how many people have "defy" in their name
-     * @param command 
+     * @param command
      */
     @Command(`defys :index`)
     @Infos({
@@ -34,7 +34,7 @@ export default abstract class fun extends baseclass {
     })
     private async defys(command: CommandMessage): Promise<void> {
         this.index_member_nicks(
-            command, 
+            command,
             {
                 nick: command.args.index || `defy`
             },
@@ -45,4 +45,59 @@ export default abstract class fun extends baseclass {
             }
         )
     }
+
+    /**
+     * Makes a magic 8ball respond to a question
+     * @param command
+     * @param question
+     */
+    @Command(`8ball :question`)
+    @Infos({
+        commandClass: commandClass,
+        args: "question",
+        description: `Answers a yes or no question using a magic 8ball`,
+        usage: `8ball <question>`
+    })
+    private async eightball(command: CommandMessage): Promise<void> {
+      if (command.args.question) {
+        let responses: Array<string> = [
+          `As I see it, yes.`,
+          `Ask again later.`,
+          `Better not tell you now.`,
+          `Cannot predict now.`,
+          `Concentrate and ask again`,
+          `Don’t count on it.`,
+          `It is certain.`,
+          `It is decidedly so.`,
+          `Most likely.`,
+          `My reply is no.`,
+          `My sources say no.`,
+          `Outlook not so good.`,
+          `Outlook good.`,
+          `Reply hazy, try again.`,
+          `Signs point to yes.`,
+          `Very doubtful.`,
+          `Without a doubt.`,
+          `Yes.`,
+          `Yes – definitely.`,
+          `You may rely on it.`,
+        ]
+      let embed = new MessageEmbed()
+      .setColor(await this.random_color())
+      .setTitle(await this.unparsed_args(command))
+      .setDescription(responses[Math.floor(Math.random() * responses.length)])
+      command.channel.send(embed)
+      return
+      }
+    /**
+     * Defaults if no question is asked
+     */
+     let embed = new MessageEmbed()
+     .setColor(await this.random_color())
+     .setTitle(`Did I forget to ask a question?`)
+     .setDescription(`Yes.`)
+     command.channel.send(embed)
+     return
+    }
+
 }
